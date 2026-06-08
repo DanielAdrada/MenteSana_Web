b201c2108b93fe1798560923cad118819225d367
Lina Lopez <linamlopezm05@gmail.com>
2026-06-08 17:07:26 -0500

    Se crea la funcion que permite al psicologo responder los comentarios de los estudiantes

--- Data/Models/CommentDTO.cs
+++ Data/Models/CommentDTO.cs
@@ -19,5 +19,7 @@ namespace Data.Models
         public string Contenido { get; set; }
         public DateTime Fecha { get; set; }
         public bool Positivo { get; set; }
+        public string Respuesta { get; set; }
+        public DateTime? FechaRespuesta { get; set; }
     }
 }
