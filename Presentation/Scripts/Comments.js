129ce99a4ebedfaad62c99205a0a6f96164808dc
Lina Lopez <linamlopezm05@gmail.com>
2026-06-08 20:28:58 -0500

    Se crea el boton para editar los comentarios del psicologo

---
+++ Presentation/Scripts/Comments.js
@@ -0,0 +1,19 @@
+﻿function mostrarEdicion(id) {
+    var div =
+        document.getElementById(
+            "editar-" + id);
+
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    }
+    else {
+        div.style.display = "none";
+    }
+}
+
+function cancelarEdicion(id) {
+    const div =
+        document.getElementById("editar-" + id);
+
+    div.style.display = "none";
+}
